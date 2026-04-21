a6efc24070dc461f44ea9428d17e058cd47d64a8
fabiopacifici <boolean@fabiopacifici.com>
2026-04-21 10:26:42 +0200

    feat: add CORS support to the application

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ const app = express();
 const bookRouter = require('./routes/books')
 const serverError = require('./middlewares/serverError')
 const notFound = require('./middlewares/notFound')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3000
 
 // register the static middleware
 app.use(express.static('public'))
+
+// Enable CORS for all routes
+app.use(cors())
+
+// Enable only for the origin http://localhost:5173
+// app.use(cors({
+//   origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+// }));
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
